06bc9d1b38b4adcd3dd671af993c9f978e7b1751
EVOFORGE\dimay <dimay@evoforge.org>
2020-07-15 16:19:12 -0400

    #15: improved splash page - WIP

--- docs/.vuepress/components/SpecialLayout.vue
+++ docs/.vuepress/components/SpecialLayout.vue
@@ -5,7 +5,7 @@
                 <div class="container">
                     <h1 class="masthead-heading mb-0"><i class="fab fa-pagelines"></i> SkillTree</h1>
                     <h2 class="masthead-subheading mb-0">Innovative approach to implementing application training</h2>
-                    <a href="overview/" class="btn btn-primary btn-xl rounded-pill mt-5">Read Docs</a>
+                    <a href="overview/" class="btn btn-lg btn-primary mt-5">Read Docs</a>
                 </div>
             </div>
             <div class="bg-circle-1 bg-circle"></div>
@@ -14,82 +14,158 @@
             <div class="bg-circle-4 bg-circle"></div>
         </header>
 
-        <section>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 order-lg-2">
-                        <div class="p-5">
-<!--                            <i class="img-fluid circle-icon fas fa-user-graduate"></i>-->
-                            <span class="fa-stack fa-4x">
-                              <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                              <i class="fa fa-user-graduate fa-stack-1x icon-foreground"></i>
-                            </span>
+        <div class="container-fluid" style="margin-top: -3rem; max-width: 1281px;">
+            <div class="row">
+                <div class="col">
+                    <div class="card h-100 card-border-top1">
+                        <div class="card-body">
+                            <div class="media">
+                                <span class="fa-stack">
+                                  <i class="fa fa-circle fa-stack-2x icon-background1"></i>
+                                  <i class="fa fa-user-graduate fa-stack-1x icon-foreground"></i>
+                                </span>
+
+                                <div class="media-body">
+                                    <h5 class="mt-0">User Engagement</h5>
+                                    Enable users to Quickly learn new features and effectively master advanced features.
+                                    Improve training recall through continued user engagement.
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 order-lg-1">
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 card-border-top2">
+                        <div class="card-body">
+                            <div class="media">
+                                <span class="fa-stack">
+                                  <i class="fa fa-circle fa-stack-2x icon-background2"></i>
+                                  <i class="fa fa-chess-king fa-stack-1x icon-foreground"></i>
+                                </span>
+
+                                <div class="media-body">
+                                    <h5 class="mt-0">Gamification</h5>
+                                    Consistent and innovative solution takes the guesswork and maintenance out of
+                                    implementing gamified user training.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100 card-border-top3">
+                        <div class="card-body">
+                            <div class="media">
+                                <span class="fa-stack">
+                                  <i class="fa fa-circle fa-stack-2x icon-background3"></i>
+                                  <i class="fa fa-money-bill-wave fa-stack-1x icon-foreground"></i>
+                                </span>
+
+                                <div class="media-body">
+                                    <h5 class="mt-0">Reduced Cost</h5>
+                                    Fewer training sessions - less support costs. Continued user engagement in
+                                    application
+                                    training promotes local experts.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <section class="mt-5 mb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
                         <div class="p-5">
-                            <h2 class="display-4">User Engagement</h2>
-                            <p>Enable users to Quickly learn new features and effectively master advanced features.
-                                Improve training recall through continued user engagement.</p>
+                            <img src="/img/home/Screenshot_2020-07-15_SkillTree_Dashboard.png" class="image-border"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5 mt-1">
+                            <h3>User Friendly</h3>
+                            <h2>Dashboard</h2>
+                            <p>A web-based user interface that enables creation and management of an applications'
+                                training profile</p>
+                            <div><i class="fas fa-check-double"></i> Effective and visual skill definition management
+                            </div>
+                            <div><i class="fas fa-check-double"></i> Comprehensive Visualization</div>
+                            <div><i class="fas fa-check-double"></i> Visibility of application expertise</div>
+                            <div><i class="fas fa-check-double"></i> SkillTree configuration management</div>
+                            <a href="dashboard/user-guide/" class="btn btn-lg btn-info mt-4">Read our Dashboard Guide</a>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
 
-        <hr/>
-
-        <section>
+        <section class="dark-bg" style="margin-top: 8rem;">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 order-lg-2" style="margin-top: -6rem;">
                         <div class="p-5">
-<!--                            <i class="img-fluid circle-icon fas fa-chess-king"></i>-->
-                            <span class="fa-stack fa-4x">
-                              <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                              <i class="fa fa-chess-king fa-stack-1x"></i>
-                            </span>
+                            <img src="/img/home/Screenshot_2020-07-15_SkillTree_ClientDisplay.png"
+                                 class="image-border-dark"/>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <h2 class="display-4">Rich Gamification Platform</h2>
-                            <p>Corporate solution takes the guesswork and maintenance out of implementing gamified user
-                                training. Provides a template that applications can use to implement the capability.</p>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5 text-white">
+                            <h3 class="">Libraries</h3>
+                            <h2 class="">Users Display</h2>
+                            <p>Pluggable user skill and ranking visualization - customize and use the display component
+                                in your application within minutes.</p>
+                            <div class="p-2">
+                                <div class="mb-2">Out-of-the-box support for:</div>
+                                <div class="ml-2 mb-2"><i class="fab fa-vuejs fa-2x"></i> Vue.js</div>
+                                <div class="ml-2 mb-2"><i class="fab fa-react fa-2x"></i> React</div>
+                                <div class="ml-2 mb-2"><i class="fab fa-angular fa-2x"></i> Anagular</div>
+                                <div class="ml-2 mb-2"><i class="fab fa-js fa-2x"></i> Pure JavaScript</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
 
-        <hr/>
 
-        <section>
+        <section class="mt-5 mb-5">
             <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 order-lg-2">
+                <div class="row">
+                    <div class="col-lg-6">
                         <div class="p-5">
-<!--                            <i class="img-fluid circle-icon fas fa-money-bill-wave"></i>-->
-
-                            <span class="fa-stack fa-4x">
-                              <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                              <i class="fa fa-money-bill-wave fa-stack-1x"></i>
-                            </span>
+                            <img src="/img/home/Screenshot_2020-07-15_Vue_js_Integration_SkillTree_Docs.png" style="max-width: 29rem"/>
                         </div>
                     </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="p-5">
-                            <h2 class="display-4">Reduced Cost</h2>
-                            <p>Fewer training sessions - less support costs. Continued user engagement in application
-                                training promotes local experts.</p>
+                    <div class="col-lg-6">
+                        <div class="p-5 mt-1">
+                            <h2>Rapid Integration</h2>
+                            <p>Quickly integrate gamified training into your web-application</p>
+                            <a href="skills-client/" class="btn btn-lg btn-info mt-3">Visit our Integration Guide</a>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="section-view-docs" class="mt-4 mb-0 p-5">
+            <div class="container">
+                <div class="row text-white">
+                    <div class="col-lg-9">
+                        <h3 class="border-bottom">Did we mention it's FREE & Open Source??</h3>
+                        <p>Read our extensive documentation to learn more about the SkillTree platform.</p>
+                    </div>
+                    <div class="col-lg-3 text-right">
+                        <a href="overview/" class="btn btn-lg btn-info">Read our Docs</a>
                     </div>
                 </div>
             </div>
         </section>
 
         <!-- Footer -->
-        <footer class="py-5 mt-4">
+        <footer class="py-5 mt-0">
             <div class="container">
                 <p class="m-0 text-center text-white small">Apache 2 Licensed | Copyright © 2019-present </p>
             </div>
@@ -106,6 +182,46 @@
 </script>
 
 <style scoped>
+
+    #section-view-docs {
+        background: #42b983;
+        position: relative;
+        overflow: hidden;
+        padding-top: calc(7rem + 72px);
+        padding-bottom: 7rem;
+        background: linear-gradient(0deg, #42b983 0%, #124430 100%);
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: scroll;
+        background-size: cover;
+    }
+
+    section h2 {
+        font-size: 3rem;
+    }
+
+    section h3 {
+        font-size: 2rem;
+    }
+
+    section i {
+        width: 2.5rem;
+    }
+
+    .image-border {
+        width: 30rem;
+        border-style: solid;
+        border-image: linear-gradient(#17a2b8, transparent) 1;
+        /*border-bottom: 0*/
+    }
+
+    .image-border-dark {
+        width: 30rem;
+        border-style: solid;
+        border-image: linear-gradient(#124430, transparent) 1;
+        /*border-bottom: 0*/
+    }
+
     body {
         font-family: 'Lato';
     }
@@ -120,35 +236,62 @@
         font-weight: 800 !important;
     }
 
-    .btn-xl {
-        text-transform: uppercase;
-        padding: 1.5rem 3rem;
-        font-size: 0.9rem;
-        font-weight: 700;
-        letter-spacing: 0.1rem;
-    }
-
-    .bg-black {
-        background-color: #000 !important;
-    }
-
-    .rounded-pill {
-        border-radius: 5rem;
-    }
-
     .fa-stack {
-        font-size: 12rem;
+        font-size: 2rem;
+        margin-right: 1rem;
+        margin-left: 1rem;
     }
 
     .icon-foreground {
-        color: #343131;
-    }
-    .icon-background {
-        color: #42b983;
+        color: #efe7e7;
     }
 
     footer {
         background: black;
+    }
+
+    .dark-bg {
+        background: #124430;
+    }
+
+    .card {
+        background: #124430;
+        border-top-leftradius: 50%;
+        border-top-right-r-adius: 50%;
+    }
+
+    .media-body {
+        color: #c1b9b9;
+    }
+
+    .media-body h5 {
+        color: #efe7e7;
+    }
+
+    .card-border-top1 {
+        border-top: 5px solid #ee0979;
+        border-top-leftradius: 50%;
+        border-top-right-r-adius: 50%;
+    }
+
+    .icon-background1 {
+        color: #ee0979;
+    }
+
+    .card-border-top2 {
+        border-top: 5px solid #007bff;
+    }
+
+    .icon-background2 {
+        color: #007bff;
+    }
+
+    .card-border-top3 {
+        border-top: 5px solid #42b983;
+    }
+
+    .icon-background3 {
+        color: #42b983;
     }
 
     section {
