be0e69c9e229b172d62063183fa45c957cb31f40
EVOFORGE\dimay <dimay@evoforge.org>
2020-07-16 16:02:09 -0400

    #15: fixing link path

--- docs/.vuepress/components/FormAndPki.vue
+++ docs/.vuepress/components/FormAndPki.vue
@@ -4,7 +4,7 @@
         <b-tabs content-class="mt-3">
             <b-tab v-for="(tabItem) of tabItems" :key="tabItem.name">
                 <div class="text-right text-secondary" style="font-size: 0.8rem;">Visit to learn about this mode:
-                    <a :href="tabItem.helpUrl" target="_blank"><span v-html="tabItem.name"></span> <i class="fas fa-external-link-alt"/></a></div>
+                    <a :href="$withBase(tabItem.helpUrl)" target="_blank"><span v-html="tabItem.name"></span> <i class="fas fa-external-link-alt"/></a></div>
                 <template v-slot:title>
                     <i :class="tabItem.icon" class="mr-1"></i> <span v-html="tabItem.name"></span>
                 </template>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -78,6 +78,6 @@ module.exports = {
         ],
         lastUpdated: 'Last Updated',
         home: true,
-        installType: 'pki',
+        installType: 'form',
     }
 }
