2617b41c7f8a44abc8a59de8d89201e37b12bccf
Dima May <sudo.may1@gmail.com>
2025-01-13 14:31:08 +0000

    #291: adjusted headers height

---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.vp-toggle-sidebar-button .icon {
+  margin-top: 0.4rem;
+}
+</style>
